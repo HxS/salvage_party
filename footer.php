ce7fbe1e2a39d95d1c493b5d6837dc6605c65412
denari <e1040anct@gmail.com>
2016-09-03 02:12:05 +0900

    add footer html

--- footer.php
+++ footer.php
@@ -1,1 +1,49 @@
-it works!
+    <footer class="pageFooter">
+      <ul class="pageFooter__links footerLinks">
+        <li class="footerLinks__link footerLinks__link--contact footerLink">
+          <div class="footerLink__title footerLink__title--text">
+            CONTACT
+          </div>
+          <small class="footerLink__descrition">
+            取材依頼・その他お問い合わせ
+          </small>
+        </li>
+        <li class="footerLinks__link footerLinks__link--foodSalvage footerLink">
+          <div class="footerLink__title footerLink__title--image">
+            <img src="<?php
+              echo get_template_directory_uri();
+              echo '/images/logo_food_salvage.png';
+            ?>" alt="Food Salvage" />
+          </div>
+          <small class="footerLink__descrition">
+            運営団体について
+          </small>
+        </li>
+      </ul>
+
+      <ul class="pageFooter__siteMap siteMap">
+        <li><a href="#">About</a></li>
+        <li><a href="#">Action</a></li>
+        <li><a href="#">Schedule</a></li>
+        <li><a href="#">Topic</a></li>
+        <li><a href="#">Report</a></li>
+        <li><a href="#">Feature</a></li>
+        <li><a href="#">Information</a></li>
+        <li><a href="#">FAQ</a></li>
+      </ul>
+    </footer>
+
+    <p class="pageFooter__note">
+      <small>
+        本ウェブサイトは、東京都環境局「持続可能な資源利用」に向けたモデル事業
+        （2015年度）として制作されました。
+      </small>
+    </p>
+    
+    <p class="pageFooter__license">
+      <small>
+        &copy; <a href="http://foodsalvage.or.jp">Food Salvage</a>
+      </small>
+    </p>
+  </body>
+</html>
