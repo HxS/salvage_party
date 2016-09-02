4fc9e3ec7ec0addd99098c7524c41f879d90aa37
denari <e1040anct@gmail.com>
2016-09-02 23:31:51 +0900

    add shell for installing plugin

--- boot.sh
+++ boot.sh
@@ -1,3 +1,4 @@
 docker-compose up -d
 docker-compose exec wordpress ln -s /share/themes/salvageparty /var/www/html/wp-content/themes/salvageparty
 docker-compose exec wordpress chown www-data:www-data /var/www/html/wp-content/themes/salvageparty
+docker-compose exec --user www-data wordpress php /share/themes/salvageparty/wp-cli.phar plugin install custom-post-type-ui --activate
