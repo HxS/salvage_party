5e4b435f8eca1d0b2fdf0d3c9f35c8c0d38d48dd
denari <e1040anct@gmail.com>
2016-09-03 01:48:31 +0900

    add header contents

--- header.php
+++ header.php
@@ -1,1 +1,42 @@
-it works!
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="<?php
+      echo get_template_directory_uri() . '/style.css';
+    ?>
+    ">
+    <title>Salvage Party</title>
+  </head>
+
+  <body>
+    <header class="pageHeader">
+      <h1>
+        <a href="#"><img src="<?php
+          echo get_template_directory_uri() . '/images/header_logo.png';
+        ?>" alt="Salvage Party" /></a>
+      </h1>
+
+      <nav class="pageHeader__navigation">
+        <ul>
+          <li><a href="#">ABOUT</a></li>
+          <li><a href="#">ACTION</a></li>
+          <li><a href="#">SCHEDULE</a></li>
+          <li><a href="#">TOPIC</a></li>
+          <li><a href="#">REPORT</a></li>
+          <li><a href="#">FEATURE</a></li>
+        </ul>
+      </nav>
+
+      <ul class="pageHeader__contactList">
+        <li><a href="#"><img src="<?php
+          echo get_template_directory_uri() . '/images/contact_mail.png';
+        ?>" alt="Salvage Party" /></a></li>
+        <li><a href="#"><img src="<?php
+          echo get_template_directory_uri() . '/images/contact_twitter.png';
+        ?>" alt="Salvage Party" /></a></li>
+        <li><a href="#"><img src="<?php
+          echo get_template_directory_uri() . '/images/contact_facebook.png';
+        ?>" alt="Salvage Party" /></a></li>
+      </ul>
+    </header>
