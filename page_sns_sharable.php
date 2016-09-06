ef84f3871932b4c3d4ec747f421144b70e25981e
denari <e1040anct@gmail.com>
2016-09-07 02:53:05 +0900

    add other templates

---
+++ page_sns_sharable.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Template Name: [固定ページ]SNSで共有可能な固定ページ
+*/
+
+get_header(); ?>
+
+<div class="mainContent mainContent--page">
+  <?php
+    global $more;
+    if(have_posts()) :
+      while(have_posts()) :
+        $more = 1;
+        the_post(); ?>
+
+        <header class="mainContent__header contentHeader">
+        	<h2 class="contentHeader__title">
+            <?php the_title(); ?>
+          </h2>
+          <p class="contentHeader__subTitle">
+            サブタイトルを表示
+          </p>
+          <?php breadcrumb(); ?>
+        </header>
+
+        <article class="mainContent__article">
+          <?php the_content(); ?>
+          <?php get_template_part('modules/module-sns'); ?>
+        </article>
+
+        <?php
+      endwhile;
+    else: ?>
+
+      <p>
+      	該当する記事が見当たりませんでした。
+      </p>
+
+    <?php endif; ?>
+
+  <?php get_sidebar(); ?>
+</div>
+
+<?php get_footer(); ?>
