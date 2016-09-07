c385509374fed4742b8350b90928cfcb885e49dd
denari <e1040anct@gmail.com>
2016-09-07 11:58:40 +0900

    add partner.php

--- partner.php
+++ partner.php
@@ -6,7 +6,7 @@ Template Name: PARTNER
 get_header(); ?>
 
 
-<div class="mainContent mainContent--action">
+<div class="mainContent mainContent--partner">
   <?php
     global $more;
     if(have_posts()) :
@@ -36,10 +36,35 @@ get_header(); ?>
 
 
     <article class="mainContent__article">
-      <!-- ここから -->
-
-      <!-- ここまで -->
       <?php the_content(); ?>
+      <!-- ここから -->
+      <h1>パートナー</h1>
+      <?php for($i=0;$i<2;$i++) { ?>
+        <ul class="partnerItemList">
+          <?php for($j=0;$j<3;$j++) { ?>
+            <li class="partnerItemList__item partnerItem">
+              <a href="#">
+                <div class='partnerItem__thumbnail' /></div>
+                企業名
+              </a>
+            </li>
+          <?php } ?>
+        </ul>
+      <?php } ?>
+      <h1>賛同企業</h1>
+      <?php for($i=0;$i<1;$i++) { ?>
+        <ul class="partnerItemList">
+          <?php for($j=0;$j<3;$j++) { ?>
+            <li class="partnerItemList__item partnerItem">
+              <a href="#">
+                <div class='partnerItem__thumbnail' /></div>
+                企業名
+              </a>
+            </li>
+          <?php } ?>
+        </ul>
+      <?php } ?>
+      <!-- ここまで -->
     </article>
   <?php get_sidebar(); ?>
 </div>
