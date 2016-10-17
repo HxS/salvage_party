f4b48ab4faba66e049f2ce923973e6420588cde8
denari <e1040anct@gmail.com>
2016-10-18 00:16:34 +0900

    remove image size

--- functions.php
+++ functions.php
@@ -753,4 +753,21 @@ function create_pages_and_setting() {
   }
 
 }
+
+add_filter( 'image_send_to_editor', 'remove_image_attribute', 10 );
+add_filter( 'post_thumbnail_html', 'remove_image_attribute', 10 );
+
+function remove_image_attribute( $html ){
+	$html = preg_replace( '/(width|height)="\d*"\s/', '', $html );
+	$html = preg_replace( '/class=[\'"]([^\'"]+)[\'"]/i', '', $html );
+	$html = preg_replace( '/title=[\'"]([^\'"]+)[\'"]/i', '', $html );
+	$html = preg_replace( '/<a href=".+">/', '', $html );
+	$html = preg_replace( '/<\/a>/', '', $html );
+	return $html;
+}
 add_action('after_setup_theme', 'create_pages_and_setting');
+
+add_action( 'admin_print_styles', 'admin_css_custom' );
+function admin_css_custom() {
+	echo '<style>.attachment-details label[data-setting="caption"], .attachment-details label[data-setting="description"], div.attachment-display-settings { display: none; }</style>';
+}
